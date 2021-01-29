9d977751bdaca552b1501319ada1d57417a91812
orel <aurelien.esnard@u-bordeaux.fr>
2021-01-29 18:23:18 +0100

    improve options

--- starthost.sh
+++ starthost.sh
@@ -8,9 +8,20 @@ function ctrl_c() { echo "Abort!" && kill -9 $$ ; }
 # Pour lister l'état de toutes les machines avec Nagios3
 # https://services.emi.u-bordeaux.fr/nagios3/cgi-bin/status.cgi?hostgroup=all
 
-# ROOM105="alia bellonda corba dama feydrautha ghanima halleck idaho lucila murbella noree odrade scytale sheeana stilgar talamane taraza tegs tuek usul"
-# ROOM204="botero buffet cassatt escher goya kandinsky lautrec legreco marquet morissot opalka redon rembrandt seurat signac tissot velasquez vlaminck watteau whistler"
-# ROOM008="beetlejuice braque cezanne chagall corot dali degas ernst gauguin leger leonardo magritte matisse millet miro modigliani monet pissarro renoir vangogh"
+#################### HOST ROOM ####################
+
+MODE="PING"
+HOSTS=""
+
+function usage() {
+    echo "Usage: $0 [options] host [...]" 
+    echo "-p : ping mode (default)"
+    echo "-b : boot mode"
+    echo "-r <room> : room number (007, 101, 102, ...)" 
+    return 0
+}
+
+#################### HOST ROOM ####################
 
 # parse JSON file to get all hosts in a room
 function getRoom()
@@ -22,61 +33,44 @@ function getRoom()
     return $RET
 }
 
-# ROOM101=$(getRoom "101")
-# ROOM104=$(getRoom "104")
-# CREMI="$ROOM104"
-# HOSTS="$HOST"
+#################### PARSE ARGS ####################
 
-MODE="PING"
-HOSTS=""
-
-function usage() {
-    echo "Usage: ..."
-}
-
-function getargs() {
-    while getopts "bpt:r:h" OPT; do
+function parseArgs() {
+    while getopts "bpr:h" OPT; do
         case $OPT in
-            p)
-                MODE="PING"
-            ;;
-            b)
-                MODE="BOOT"
-            ;;
-            t)
-		local HOST="$OPTARG"
-                HOSTS+=" $HOST"
-            ;;
-            r)
-                local ROOM="$OPTARG"
-                HOSTS+=$(getRoom "$ROOM")
-            ;;
-            h)
-                usage
-            ;;
-            \?)
-                echo "Invalid option!"
-                usage
-            ;;
+            p ) MODE="PING" ;;
+            b ) MODE="BOOT" ;;
+            r ) HOSTS+=$(getRoom "$OPTARG") ;;
+            h ) usage && exit 0 ;;
+            * ) usage && exit 1 ;;
         esac
     done
 
+    shift $((OPTIND-1))
+    # echo "$# other args: $*"
 
-    # check args
-    if [ $# -eq 0 ] ; then usage ; fi
-    if [ -z "$MODE" ] ; then usage ; fi
-    echo "Mode:$MODE"
+    # [ ! $# -ge 1 ] && usage && exit 1 
+    HOSTS=$(echo "$HOSTS $*" | xargs)
+    echo "Target Hosts: \"$HOSTS\""
+    [ -z "$HOSTS" ] && echo "Error: no target hosts!" && usage && exit 1
+    # [ ! $# -ge 1 ] && usage && exit 1 
 
+    
     return 0
 }
+
+# The difference between $* and $@ comes in how they are expanded.
+# 1) $* expands to a single argument with all the elements delimited
+# by spaces (actually the first character of $IFS).
+# 2) $@ expands to multiple arguments.
 
 #################### PING MODE ####################
 
 function mping() {
-    echo "===== Host Monitoring ====="
+    echo "===== Ping Hosts ====="
     local HOSTS="$*"
-    echo "Target Hosts: $HOSTS"
-    echo "ping..."
+    # echo "Target Hosts: $HOSTS"
+    echo "Ping..."
     fping $HOSTS
     return 0
 }
@@ -84,7 +78,7 @@ function mping() {
 #################### BOOT MODE ####################
 
 function mboot() {
-    echo "===== Host Starting ====="
+    echo "===== Boot Hosts ====="
     local HOSTNAME="$1"
     grep -w "\"$HOSTNAME\"" hosts.json &> /dev/null
     [ ! $? -eq 0 ] && echo "Error: hostname not found!" && exit 1
@@ -110,13 +104,14 @@ function mboot() {
 # ssh -o "StrictHostKeyChecking=no" $HOSTNAME loginctl
 
 # test windows !!!
-#  nmap -Pn -6 cezanne
-# =>les ports 139 et 445 sont ouverts, le poste est sous windows
+#  nmap -PN -n -6 cezanne
+# => les ports 139 et 445 sont ouverts, le poste est sous windows
 
 
 #################### MAIN ####################
 
-getargs $*
+parseArgs $* # || exit 1
+echo "Mode: $MODE"
 
 [ "$MODE" = "PING" ] && mping $HOSTS
 [ "$MODE" = "BOOT" ] && mboot $HOSTS
